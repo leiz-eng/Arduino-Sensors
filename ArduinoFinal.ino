b4778cc18009871e713d6b4c1322859b9418025b
15735153915@163.com <46803831+LeiZhao12@users.noreply.github.com>
2023-08-23 16:56:40 +0800

    add servo and ultrasonic sensor, protothreads
    Signed-off-by: 15735153915@163.com <46803831+LeiZhao12@users.noreply.github.com>

--- ArduinoFinal.ino
+++ ArduinoFinal.ino
@@ -5,24 +5,53 @@
 #include <IRremote.hpp>
 #include <Wire.h>
 #include "RtcDS1307.h"  //DS1307时钟模块的库
+#include <Servo.h>
+#include <SR04.h>
+
+#include "protothreads.h"
+
 
 /*
   GPIO Arrange
   D2 OUT 3W LED
   D3 OUT fan
   D4 OUT buzzer
-
+  D5 IN  IR RECEIVE pin
+  D7 & 8 Ultrasonic sensor
+  D9 OUT Servo
 
 */
 
 #define IR_RECEIVE_PIN 5
 
+int led_pin = 2;
 int fan_pin = 3;
 int buzzer_pin = 4;
-int ps_pin = 5; 
-int led_pin = 2;
+
 RtcDS1307<TwoWire> Rtc(Wire);// i2c接口
 
+Servo myservo; // servo variable
+SR04 sr04 = SR04(7,8); // Ultrasonic initialization
+
+pt ptServo;
+bool servo_rotate = false; // for incoming serial data
+int servoThread(struct pt* pt) {
+  PT_BEGIN(pt);
+
+  // Loop forever
+  for(;;) {
+    if (servo_rotate){
+      myservo.write(0);//设置舵机旋转的角度
+      PT_SLEEP(pt, 500);
+      myservo.write(90);//设置舵机旋转的角度
+      PT_SLEEP(pt, 500);
+    } else {
+      PT_YIELD(pt);
+    }
+  }
+
+  PT_END(pt);
+}
 
 // notes in the melody:
 int melody[] = {
@@ -41,12 +70,13 @@ void setup() {
   pinMode(fan_pin, OUTPUT);
   pinMode(buzzer_pin, OUTPUT);
   pinMode(led_pin, OUTPUT);
+  myservo.attach(9);//定义舵机接口（9、10 都可以，缺点只能控制2 个）
+  PT_INIT(&ptServo);
   // ps sensor
   // pinMode(ps_pin, INPUT);
   // attachInterrupt(0, ps_func, RISING);
 
   // DS1307
-
   Serial.println("Serial begin");
   Rtc.Begin();
   Serial.println("begin");
@@ -75,6 +105,8 @@ void loop() {
      * address is in command is in IrReceiver.decodedIRData.address
      * and up to 32 bit raw data in IrReceiver.decodedIRData.decodedRawData
      */
+  PT_SCHEDULE(servoThread(&ptServo));
+
   if (IrReceiver.decode()) {
     /*
          * Print a short summary of received data
@@ -113,16 +145,27 @@ void loop() {
       digitalWrite(led_pin, HIGH);
     } else if (IrReceiver.decodedIRData.command == 0x15) {
       digitalWrite(led_pin, LOW);
+    } else if (IrReceiver.decodedIRData.command == 0x43) {
+      servo_rotate = !servo_rotate;
+      Serial.print("servo_rotate: ");
+      Serial.println(servo_rotate);
+    } else if (IrReceiver.decodedIRData.command == 0x46) {
+
     }
     IrReceiver.resume();  // Enable receiving of the next value
   }
-  Serial.print(Rtc.GetDateTime().Year());
-  Serial.print("    ");
-  Serial.print(Rtc.GetDateTime().Month());
-  Serial.print(Rtc.GetDateTime().Second());
-  Serial.print("    ");
-  Serial.println(Rtc.GetDateTime().DayOfWeek());
-  delay(1000);//延时1秒
+  float distance = sr04.Get();
+
+  // Serial.print(distance);
+  // Serial.print("cm\n");
+  // delay(500);
+  // Serial.print(Rtc.GetDateTime().Year());
+  // Serial.print("    ");
+  // Serial.print(Rtc.GetDateTime().Month());
+  // Serial.print(Rtc.GetDateTime().Second());
+  // Serial.print("    ");
+  // Serial.println(Rtc.GetDateTime().DayOfWeek());
+  // delay(1000);//延时1秒
 }
 
 void newtone(byte tonePin, int frequency, int duration) {
--- README.md
+++ README.md
@@ -1,10 +1,28 @@
 "# Arduino-Sensors" 
 
 This repo is about using infrared sensor to control some electronic components which includes 3W LED, buzzer, DC generator and so on. I will try add more into it.
-
+***
 Hardwares:
 1. Arduino
 2. 3W LED
 3. buzzer
 4. DC generator
+5. Servo
+6. Ultrasonic
+***
+Software libs:
+1. IRremote
+2. DS1307
+3. Protothreads  
+this lib is used for miltitask running because you cannot put a delay function into loop() function, it will influence other features
+4. Ultrasonic SR04
 
+***
+GPIO Arrange  
+D2 OUT 3W LED  
+D3 OUT fan  
+D4 OUT buzzer  
+D5 IN  IR RECEIVE pin  
+D7 & 8 Ultrasonic sensor  
+D9 OUT Servo  
+***
